6c8bd655da94f428004daf10ee7d9d5891ee2061
LongNHH <fvn.longnhh@gmail.com>
2019-12-24 21:16:26 +0700

    box07

--- js/main.js
+++ js/main.js
@@ -1,105 +1,29 @@
 $(document).ready(function() {
     "use strict";
 
-    $('.box02_list ul').slick({
-        dots: false,
-        arrows: false,
+    $('.box06_list ul').slick({
+        dots: true,
+        arrows: true,
         slidesToShow: 2,
         speed: 500,
         pauseOnHover: false,
         pauseOnFocuse: false,
         margin: 0,
-        autoplay: false,
+        centerMode: true,
+        centerPadding : '50px',
+        autoplay: true,
         autoplaySpeed: 5000,
         infinite: false,
         responsive: [
             {
               breakpoint: 769,
               settings: {
-                slidesToShow: 1,
-              }
-            }
-        ]
-    });
-
-    $('.box03_list').slick({
-        dots: false,
-        arrows: false,
-        slidesToShow: 5,
-        margin: 0,
-        pauseOnHover: false,
-        pauseOnFocuse: false,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-            {
-              breakpoint: 1201,
-              settings: {
-                slidesToShow: 4,
-              }
-            },
-            {
-              breakpoint: 769,
-              settings: {
-                slidesToShow: 3,
-              }
-            },{
-              breakpoint: 481,
-              settings: {
                 slidesToShow: 2,
               }
             }
-        ]
-
-    });
-
-
-    $('.box04_txt ul').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: false,
-        fade: true,
-        asNavFor: '.box04_img ul',
-        prevArrow: '<button type="button" class="slick-prev btn"><a></a></button>',
-        nextArrow: '<button type="button" class="slick-next btn"><a></a></button>'
-    });
-
-    $('.box04_img ul').slick({
-        slidesToShow: 11,
-        slidesToScroll: 1,
-        asNavFor: '.box04_txt ul',
-        dots: false,
-        arrows: false,
-        centerMode: true,
-        centerPadding: '0',
-        focusOnSelect: true,
-        responsive: [
-            {
-              breakpoint: 1201,
-              settings: {
-                slidesToShow: 9,
-              }
-            },
-            {
-              breakpoint: 769,
-              settings: {
-                slidesToShow: 7,
-              }
-            },
-            {
-              breakpoint: 481,
-              settings: {
-                slidesToShow: 5,
-              }
-            },
-            {
-              breakpoint: 361,
-              settings: {
-                slidesToShow: 3,
-              }
-            }
-        ]
+        ],
+        prevArrow: '<button type="button" class="slick-prev"><a><img src="./images/ic_prev.jpg"></a></button>',
+        nextArrow: '<button type="button" class="slick-next"><a><img src="./images/ic_next.jpg"></a></button>'
     });
 
     AOS.init({
@@ -115,7 +39,7 @@ $(window).bind('load',function(){
     $('a[href^="#"]').click(function(){
       if ($($(this).attr('href') ).length ) {
         var p = $( $(this).attr('href') ).offset();
-        if($(window).width() > 640){
+        if($(window).width() > 768){
           $('html,body').animate({ scrollTop: p.top - 100}, 400);
         }
         else {
